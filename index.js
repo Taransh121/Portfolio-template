1914f4cb0506fb3bb917cd6d3db2ef138441dd62
TARANSH CHELLANI <taranshchellani121@gmail.com>
2022-12-09 01:11:38 +0530

    DoNe

--- index.js
+++ index.js
@@ -41,12 +41,12 @@ setInterval(() => {
 }, 2000)
 
 //Modal
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', () => {
-    myInput.focus()
-})
+// myModal.addEventListener('shown.bs.modal', () => {
+//     myInput.focus()
+// })
 
 
 
